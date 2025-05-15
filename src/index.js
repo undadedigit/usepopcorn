3b32b78794f01b384885794b39540b4377359222
Anthony Rana <undadedigit@users.noreply.github.com>
2025-05-15 14:26:46 +0100

    Wrong way to Fetch data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
@@ -19,7 +19,7 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     <StarRating
       maxRating={5}
       messages={["terrible", "bad", "ok", "good", "great"]}
