70e745b216e15e3020f00e832ea16fc941c85ea7
Anthony Rana <undadedigit@users.noreply.github.com>
2025-05-22 11:55:46 +0100

    Minor edits

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from "./App";
 
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -25,6 +25,5 @@ root.render(
       messages={["terrible", "bad", "ok", "good", "great"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
   </React.StrictMode>
 );
