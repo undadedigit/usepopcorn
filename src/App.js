a2a88fe0f76691fd002ea4c5cc76d988bfa9bff3
Anthony Rana <undadedigit@users.noreply.github.com>
2025-05-29 13:53:25 +0100

    Listening to a Keypress

--- src/App.js
+++ src/App.js
@@ -332,6 +332,22 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(
     function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
+  useEffect(
+    function () {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
